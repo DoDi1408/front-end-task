9782c906f9cfe498f4fe78fe6d2af64eb4d17ba9
Riku-li <rickyweringo6@gmail.com>
2024-05-30 00:16:02 -0600

    Fixed the display of error messages and made some formatting changes

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -17,43 +17,61 @@ const Login = () => {
   const isValidEmail = (email: string) =>
     /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-  const login = () => {
+  const login = async () => {
     if (!isValidEmail(email)) {
-      setError("Invalid email, please try again.");
+      setError("Invalid, please try again.");
       return;
     }
 
     if (email === "manager@oracle.com") {
-      localStorage.setItem("userId", "22");
       navigate("/manager");
     } else if (email === "employee@oracle.com") {
-      localStorage.setItem("userId", "31");
       navigate("/employee");
     } else {
-      setError("Invalid, please try again.");
+      setError("Invalid role, please try again.");
     }
   };
 
-  const handleRegister = () => {
-    console.log("Registration Data:", { email, password, telegramId });
+  const handleRegister = async () => {
+    if (!isValidEmail(email)) {
+      setError("Invalid, please try again.");
+      return;
+    }
+
+    if (email === "manager@oracle.com") {
+      navigate("/manager");
+    } else if (email === "employee@oracle.com") {
+      navigate("/employee");
+    } else {
+      setError("Invalid role, please try again.");
+    }
+  };
+
+  const toggleRegistering = () => {
+    setIsRegistering(!isRegistering);
+    setError("");
+    setEmail("");
+    setPassword("");
+    setTelegramId("");
   };
 
   return (
     <div className="flex justify-center items-start h-screen bg-gradient-to-r from-red-50 via-white-50 to-sky-50 pt-[150px]">
-      <div className="bg-white flex flex-col w-3/5 h-2/5 items-center justify-center grid grid-cols-2 rounded-md shadow-2xl">
+      <div className="bg-white flex flex-col w-3/5 h-auto items-center justify-center grid grid-cols-2 rounded-md shadow-2xl">
         <div className="text-center bg-red-400 h-full w-full justify-center flex flex-col rounded-l-md">
           <p className="font-bold text-2xl text-white">Welcome!</p>
           <img src={logo} className="w-72 mt-6 mx-auto" />
         </div>
-        <div className="self-center justify-self-center text-center px-8">
+        <div className="self-center justify-self-center text-center px-8 py-8">
           {isRegistering ? (
             <>
-              <p className="font-bold text-2xl my-8">Register</p>
-              <div className="grid my-8 w-full max-w-sm gap-1.5">
+              <p className="font-bold text-2xl my-4">Register</p>
+              <div className="grid mb-6 w-full max-w-sm gap-1.5">
                 <Input
                   type="email"
                   id="email"
                   placeholder="Email"
+                  value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
                 <Label htmlFor="password" className="mb-1"></Label>
@@ -61,6 +79,7 @@ const Login = () => {
                   type="password"
                   id="password"
                   placeholder="Password"
+                  value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
                 <Label htmlFor="telegramId" className="mb-1"></Label>
@@ -68,6 +87,7 @@ const Login = () => {
                   type="text"
                   id="telegramId"
                   placeholder="Telegram ID"
+                  value={telegramId}
                   onChange={(e) => setTelegramId(e.target.value)}
                 />
               </div>
@@ -80,24 +100,34 @@ const Login = () => {
                 </Button>
                 <Button
                   className="py-2 px-4 rounded border border-red-500 text-red-500 bg-white hover:bg-red-100"
-                  onClick={() => setIsRegistering(false)}
+                  onClick={toggleRegistering}
                 >
                   Go Back
                 </Button>
               </div>
+              {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
             </>
           ) : (
             <>
-              <p className="font-bold text-2xl my-8">Login</p>
-              <div className="grid my-8 w-full max-w-sm gap-1.5">
+              <p className="font-bold text-2xl my-4">Login</p>
+              <div className="grid mb-6 w-full max-w-sm gap-1.5">
                 <Input
                   type="email"
                   id="email"
                   placeholder="Email"
+                  value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   onKeyPress={(event) => event.key === "Enter" && login()}
                 />
-                {error && <p className="text-red-500 mt-2">{error}</p>}
+                <Label htmlFor="password" className="mb-1"></Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
               </div>
               <div className="flex justify-between mt-4">
                 <Button
@@ -109,7 +139,7 @@ const Login = () => {
                 <div className="flex flex-col items-center justify-self-end ml-auto">
                   <Button
                     className="py-2 px-4 rounded border border-red-500 text-red-500 bg-white hover:bg-red-100 ml-auto"
-                    onClick={() => setIsRegistering(true)}
+                    onClick={toggleRegistering}
                   >
                     Register Now
                   </Button>
