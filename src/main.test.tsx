dd7292286694096f82b41ff5587f72adfd235f74
Mateo Bernasconi Vargas <bernasconivargas.mateo@gmail.com>
2024-06-11 18:48:43 -0700

    added more unit tests

--- src/main.test.tsx
+++ src/main.test.tsx
@@ -2,6 +2,7 @@ import { mapStateTaskToColumn } from "./components/employeeKanban/kanban";
 import { render, waitFor } from "@testing-library/react";
 import axios from 'axios';
 import EmployeeDashboard from "./routes/employeeDashboard";
+import ManagerDashboard from "./routes/managerDashboard";
 import { act } from "react-dom/test-utils";
 jest.mock("axios"); // Mock axios for controlled testing
 
@@ -22,7 +23,7 @@ test("Test task state mapping for kanban view", () => {
 
 
 
-describe('App', () => {
+describe('Employee Dashboard', () => {
   const mockedAxios = axios as jest.Mocked<typeof axios>;
 
   beforeEach(() => {
@@ -66,6 +67,114 @@ describe('App', () => {
 
       expect(getByText("My Tasks")).toBeInTheDocument();
       expect(getByText("Task 1")).toBeInTheDocument();
+    })
+  });
+
+  test("Employee Dashboard handles 0 tasks correctly", async () => {
+  
+    const mockResponse = {
+      data: [],
+      headers: {
+        "new-token": "updated_token",
+      },
+    };
+  
+    mockedAxios.get.mockResolvedValue(mockResponse)
+
+    const {getByText} = await act( async () => {
+     return render(<EmployeeDashboard axiosInstance={mockedAxios}/>)
+  });
+  
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith("https://api.romongo.uk/employee/tasks",  {"headers": {"token": "test-token"}});
+
+      expect(getByText("No results.")).toBeInTheDocument();
+    })
+  });
+})
+
+describe('Manager Dashboard', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  const mockData = [
+    {
+      id: 1,
+      title: "Task 1",
+      description: "This is a task description",
+      stateTask: "To Do",
+      dueDate: "2024-06-12",
+      employee: {
+        firstName: "Juan",
+        lastName: "Perez"
+      }
+    },
+  ];
+  const mockResponse = {
+    data: mockData,
+    headers: {
+      "new-token": "updated_token",
+    },
+  };
+
+  beforeEach(() => {
+    // Set the token in local storage
+    window.localStorage.setItem("jwt", "test-token");
+  });
+
+  afterEach(() => {
+    // Clean up local storage
+    window.localStorage.removeItem("jwt");
+  });
+
+  test("Manager Dashboard fetches data on mount", async () => {
+  
+    mockedAxios.get.mockResolvedValue(mockResponse)
+
+    await act( async () => {
+      render(<ManagerDashboard />)
+  });
+  
+    
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith("https://api.romongo.uk/manager/teamTasks",  {"headers": {"token": "test-token"}});
+
+    })
+  });
+
+  test("Verify employees names are parsed and shown correctly", async () => {
+  
+    mockedAxios.get.mockResolvedValue(mockResponse)
+
+    const {getByText} = await act( async () => {
+      return render(<ManagerDashboard />)
+  });
+  
+    
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith("https://api.romongo.uk/manager/teamTasks",  {"headers": {"token": "test-token"}});
+      expect(getByText("Juan Perez")).toBeInTheDocument();
+    })
+  });
+
+  test("Handle 0 team employee tasks correctly", async () => {
+    
+    const mockResponse2 = {
+      data: [],
+      headers: {
+        "new-token": "updated_token",
+      },
+    };
+
+    mockedAxios.get.mockResolvedValue(mockResponse2)
+
+    const {getByText} = await act( async () => {
+      return render(<ManagerDashboard />)
+  });
+  
+    
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith("https://api.romongo.uk/manager/teamTasks",  {"headers": {"token": "test-token"}});
+      
+      expect(getByText("No results.")).toBeInTheDocument();
     })
   });
 })
